68860eeea396b7a23bc4734de81e97abbcb5b201
Artem Trofymenko <randallsng@gmail.com>
2022-12-30 17:53:42 +0100

    commit :)

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame frame  = new JFrame("Dendy Tanks");
+        frame.setMinimumSize(new Dimension(576,576));
+        frame.setVisible(true);
+
+    }
+}
